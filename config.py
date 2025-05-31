bc5de195cff4f0fdc1bba14474fbc13173eda014
uddaniiii <danyou2260@gmail.com>
2025-05-31 14:29:34 +0900

    feat: separate MODEL_NAME and MODEL_PATH, update save and output paths

--- config.py
+++ config.py
@@ -7,11 +7,14 @@ TRAIN_DIR = "C:/uddaniiii/used-car-image-classification/data/train"
 TEST_DIR = "C:/uddaniiii/used-car-image-classification/data/test"
 
 IMG_SIZE = 224
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 EPOCHS = 100
 LR = 1e-4
 SEED = 42
 NUM_WORKERS = 0
 
-MODEL_NAME = "test"
-SAVE_DIR = f"./checkpoints/{MODEL_NAME}"
+MODEL_NAME= "tiny_vit_11m_224"
+MODEL_PATH = "tiny_vit_11m_224_cutmix"
+SAVE_DIR = f"./checkpoints/{MODEL_PATH}"
+
+OUTPUT_CSV = f"./submit/{MODEL_PATH}.csv"
