42354a7b878518dd0e07b35d7fd0afb5fefb6b12
uddaniiii <danyou2260@gmail.com>
2025-06-29 10:51:46 +0900

    feat: add loss param

--- config.py
+++ config.py
@@ -6,15 +6,17 @@ import torch
 TRAIN_DIR = "C:/uddaniiii/used-car-image-classification/data/train"
 TEST_DIR = "C:/uddaniiii/used-car-image-classification/data/test"
 
-IMG_SIZE = 224
-BATCH_SIZE = 64
-EPOCHS = 100
+IMG_SIZE = 384
+BATCH_SIZE = 16
+EPOCHS = 150
+LOSS = "ce"
 LR = 1e-4
 SEED = 42
 NUM_WORKERS = 0
 
-MODEL_NAME= "tiny_vit_11m_224"
-MODEL_PATH = "tiny_vit_11m_224_cutmix"
+MODEL_NAME= "tiny_vit_21m_384"
+MODEL_PATH = "tiny_vit_21m_384_randaug_hem"
 SAVE_DIR = f"./checkpoints/{MODEL_PATH}"
 
 OUTPUT_CSV = f"./submit/{MODEL_PATH}.csv"
+OUTPUT_NPY = f"./npy/{MODEL_PATH}.npy"
